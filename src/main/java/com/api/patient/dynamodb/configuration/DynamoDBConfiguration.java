865a766838de164622d795db9a11cb39a5ebed7b
yaswanthsridhar <ysridhar@balsambrands.com>
2022-11-17 16:06:37 +0530

    Initial commit

---
+++ src/main/java/com/api/patient/dynamodb/configuration/DynamoDBConfiguration.java
@@ -0,0 +1,2 @@
+package com.api.patient.dynamodb.configuration;public class DynamoDBConfiguration {
+}
---
+++ src/test/java/com/api/patient/dynamodb/DynamoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.patient.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
