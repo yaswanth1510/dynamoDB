5017a0954889ef68ea6f52c14a01fb377667af31
yaswanthsridhar <ysridhar@balsambrands.com>
2022-11-17 18:55:10 +0530

    Added swagger api documentation to the project.

--- src/main/java/com/api/patient/dynamodb/DynamoDbApplication.java
+++ src/main/java/com/api/patient/dynamodb/DynamoDbApplication.java
@@ -2,9 +2,10 @@ package com.api.patient.dynamodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication(scanBasePackages = { "com.api.patient.dynamodb"})
+@EnableOpenApi
 public class DynamoDbApplication {
 
     public static void main(String[] args) {
